62dd1219d0bece6cb4fe5f9d8a98c531a561a194
ShubhamSPawde <shubhamspawade@gmail.com>
2024-12-05 11:16:59 +0530

    Create Day 21 - Sort 0s, 1s and 2s

---
+++ Sorting/Day 21 - Sort 0s, 1s and 2s.cpp
@@ -0,0 +1,76 @@
+/* https://www.geeksforgeeks.org/batch/gfg-160-problems/track/sorting-gfg-160/problem/sort-an-array-of-0s-1s-and-2s4231
+
+Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
+
+Examples:
+
+Input: arr[] = [0, 1, 2, 0, 1, 2]
+Output: [0, 0, 1, 1, 2, 2]
+Explanation: 0s 1s and 2s are segregated into ascending order. */
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        int cnt1 = 0, cnt2 = 0, cnt0 = 0;
+        for(int i = 0; i<arr.size(); i++){
+            if(arr[i] == 0){
+                cnt0++;
+            }else if(arr[i] == 1){
+                cnt1++;
+            }else{
+                cnt2++;
+            }
+        }
+        int k = 0;
+        while(cnt0--){
+            arr[k++] = 0;
+        }
+        while(cnt1--){
+            arr[k++] = 1;
+        }
+        while(cnt2--){
+            arr[k++] = 2;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> a;
+        string input;
+        int num;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+        Solution ob;
+        ob.sort012(a);
+
+        int n = a.size();
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
