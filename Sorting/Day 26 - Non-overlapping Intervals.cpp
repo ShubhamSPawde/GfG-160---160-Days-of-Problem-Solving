c84c6e45676a4fb63f03e85b0f30857956d73bd9
ShubhamSPawde <shubhamspawade@gmail.com>
2024-12-10 20:04:02 +0530

    Create Day 26 - Non-overlapping Intervals.cpp

--- Sorting/Day 26 - Non-overlapping Intervals.cpp
+++ Sorting/Day 26 - Non-overlapping Intervals.cpp
@@ -1,4 +1,4 @@
-https://www.geeksforgeeks.org/batch/gfg-160-problems/track/sorting-gfg-160/problem/non-overlapping-intervals
+/* https://www.geeksforgeeks.org/batch/gfg-160-problems/track/sorting-gfg-160/problem/non-overlapping-intervals
 
 Given a 2D array intervals[][] of representing intervals where intervals [i] = [starti, endi ]. Return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
 
@@ -10,7 +10,7 @@ Explanation: [1, 3] can be removed and the rest of the intervals are non-overlap
 
 Input: intervals[][] = [[1, 3], [1, 3], [1, 3]]
 Output: 2
-Explanation: You need to remove two [1, 3] to make the rest of the intervals non-overlapping. 
+Explanation: You need to remove two [1, 3] to make the rest of the intervals non-overlapping. */
 
 //{ Driver Code Starts
 #include <bits/stdc++.h>
