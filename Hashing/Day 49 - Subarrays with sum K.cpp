0eb62c87b660f83555e34484d0d000009a050326
ShubhamSPawde <shubhamspawade@gmail.com>
2025-01-02 12:41:17 +0530

    Create Day 49 - Subarrays with sum K.cpp

---
+++ Hashing/Day 49 - Subarrays with sum K.cpp
@@ -0,0 +1,72 @@
+/* https://www.geeksforgeeks.org/batch/gfg-160-problems/track/hashing-gfg-160/problem/subarrays-with-sum-k
+
+Given an unsorted array of integers, find the number of continuous subarrays having sum exactly equal to a given number k.
+
+Examples:
+
+Input: arr = [10, 2, -2, -20, 10], k = -10
+Output: 3
+Explaination: Subarrays: arr[0...3], arr[1...4], arr[3...4] have sum exactly equal to -10.
+
+Input: arr = [9, 4, 20, 3, 10, 5], k = 33
+Output: 2
+Explaination: Subarrays: arr[0...2], arr[2...4] have sum exactly equal to 33. */
+
+
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int countSubarrays(vector<int> &arr, int k) {
+        unordered_map<int, int> mpp;
+        mpp[0] = 1;
+        int presum = 0;
+        int cnt = 0;
+        for(int i = 0; i<arr.size(); i++){
+            presum += arr[i];
+            int remo = presum - k;
+            cnt += mpp[remo];
+            mpp[presum] += 1;
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        cout << obj.countSubarrays(arr, k);
+        cout << endl;
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
